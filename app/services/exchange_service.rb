8bbde80f78251e8430217f72f60ac11082ad00b2
Pedro Schmitt <pedromartins.bsb@gmail.com>
2018-12-11 13:13:34 -0200

    update credentials

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -8,7 +8,7 @@ class ExchangeService
     @amount = amount.to_f
   end
 
-
+ 
   def perform
     begin
       exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
@@ -16,10 +16,10 @@ class ExchangeService
       url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
       res = RestClient.get url
       value = JSON.parse(res.body)['currency'][0]['value'].to_f
-
+      
       value * @amount
     rescue RestClient::ExceptionWithResponse => e
-      e.response      
+      e.response
     end
   end
-end  
+end
