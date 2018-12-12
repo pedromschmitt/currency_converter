48bd752f6a790410dcc7853365aea1a32447c339
Pedro Schmitt <pedromartins.bsb@gmail.com>
2018-12-12 17:10:08 -0200

    Create system test

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -3,7 +3,7 @@ class ExchangeController < ApplicationController
   end
 
   def convert
-    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:value]).perform
+    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
     render json: {"value": value}
   end
 end
