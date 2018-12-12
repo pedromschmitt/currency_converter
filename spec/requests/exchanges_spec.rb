e77ca204a0c9aef5b4e00249070aa4e1fc6af879
Pedro Schmitt <pedromartins.bsb@gmail.com>
2018-12-12 10:38:44 -0200

    Create rspec test for exchange

--- spec/requests/exchanges_spec.rb
+++ spec/requests/exchanges_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Exchanges", type: :request do
-  describe "GET /exchanges" do
-    it "works! (now write some real specs)" do
-      get exchanges_index_path
+
+RSpec.describe 'Exchanges', type: :request do
+  describe 'GET #index' do
+    it 'returns http success' do
+      get '/'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  
+  describe 'GET #convert' do
+    before do
+      @amount = rand(1..9999)
+    end
+
+    it 'returns http success' do
+      get '/convert', params: {
+                        source_currency: "USD",
+                        target_currency: "BRL",
+                        amount: @amount
+                      }
       expect(response).to have_http_status(200)
     end
   end
